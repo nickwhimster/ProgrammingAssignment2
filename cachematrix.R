553ebd872b075bc4361f5db8f91be51521d63fbc
nick whimster <nickwhimster@gmail.com>
2015-01-24 18:48:15 +0000

    addition of functions
    two functions added, makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function creates an object which has two properties
+## x - a matix and i - the inverse of x.
+## these properties are set or returned by the four functions in 
+## this object.
+## if the set function is called the inversed property will be deleted
+## and the next time the getsolve function is called it will be created.
+##The properties of this object are not available outside
+## of this object.
 
+makeCacheMatrix<- function(x = matrix()) {
+    i <- NULL
+    set <- function(y) {
+        x<<-y
+        i<<-NULL
+    }
+    get <-function() x
+    setsolve <- function(solve) i <<- solve
+    getsolve <- function() i
+    list(set =set, get = get,
+         setsolve= setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## this function takes and object created from the function makeCacheMatrix
+## and creates and returns the inverse of the matrix in the object.
+## If the the inverse already exists it will just use the getSolve funtction
+## to return the existing inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getsolve()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setsolve(i)
+    i
 }
